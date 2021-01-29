89329fb47c259ec6d799e0ad1201286f7fe36ced
croudit <croudit@pop-os.localdomain>
2021-01-29 16:37:12 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-center">Hello Nuxt</h1>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ vuetify.options.js
@@ -0,0 +1,19 @@
+import UserIcon from "~/components/icons/UserIcon.vue";
+import GroupIcon from "~/components/icons/GroupIcon.vue";
+import LocationIcon from "~/components/icons/LocationIcon.vue";
+
+export default {
+  icons: {
+    values: {
+      user: {
+        component: UserIcon
+      },
+      group: {
+        component: GroupIcon
+      },
+      location: {
+        component: LocationIcon
+      }
+    }
+  }
+};
