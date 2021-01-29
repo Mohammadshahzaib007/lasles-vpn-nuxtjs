ca844dc657b33be1cf140148116f0427d2f0e583
croudit <croudit@pop-os.localdomain>
2021-01-29 20:12:05 +0530

    home page has completed half

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,57 @@
-<template></template>
+<template>
+  <v-container>
+    <!-- Section first -->
+    <section
+      style="
+        height: 35.8706rem;
+        margin-top: 5.5619rem;
+        margin-bottom: 6.4375rem;
+      "
+    >
+      <v-row justify="center" align="center">
+        <v-col cols="6">
+          <h1 style="font-size: 3.125rem; font-weight: 500">
+            Want anything to be <br />
+            easy with
+            <p
+              style="
+                font-size: 3.125rem;
+                font-weight: 700;
+                display: inline-block;
+              "
+            >
+              LaslesVPN.
+            </p>
+          </h1>
+          <p class="mt-5 mb-11" style="font-weight: 500; color: #4f5665">
+            Provide a network for all your needs with ease and fun using
+            LaslesVPN discover interesting features from us.
+          </p>
+          <v-btn
+            width="15.625rem"
+            height="3.75rem"
+            color="primary"
+            class="text-capitalize py-4 px-19"
+            style="
+              font-weight: 700;
+              line-height: 19px;
+              box-shadow: 0px 20px 54px rgba(245, 56, 56, 0.35);
+            "
+          >
+            Get Started
+          </v-btn>
+        </v-col>
+        <v-col cols="6">
+          <v-img
+            :src="require('@/assets/images/Illustration1.png')"
+            height="23.9025rem"
+            width="38.2025rem"
+          />
+        </v-col>
+      </v-row>
+    </section>
+  </v-container>
+</template>
 
 <script>
 export default {};
